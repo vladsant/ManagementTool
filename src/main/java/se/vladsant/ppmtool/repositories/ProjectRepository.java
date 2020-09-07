8b5c51b3511abe599bf5261a59def6389deda54d
VladSant <vladisant@gmail.com>
2020-09-07 11:35:53 +0200

    project object and repo

---
+++ src/main/java/se/vladsant/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package se.vladsant.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import se.vladsant.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+	
+}
