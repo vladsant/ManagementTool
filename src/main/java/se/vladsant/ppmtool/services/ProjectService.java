1b01654493a1b3cc121fe4e6c8dcb5f25fc7e652
VladSant <vladisant@gmail.com>
2020-09-09 08:39:05 +0200

    Custom exception

--- src/main/java/se/vladsant/ppmtool/services/ProjectService.java
+++ src/main/java/se/vladsant/ppmtool/services/ProjectService.java
@@ -4,16 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import se.vladsant.ppmtool.domain.Project;
+import se.vladsant.ppmtool.exceptions.ProjectIdException;
 import se.vladsant.ppmtool.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
-	
+
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException(
+					"Project ID '" + project.getProjectIdentifier().toUpperCase() + "' alredy exist");
+
+		}
+
 	}
 
 }
